c16b4f6e92b1a04ed273e850f0600c7eb59ea6b3
Catelyn-Close <catie.did75@gmail.com>
2024-08-23 12:07:52 -0700

    exported from Skill Struck

--- index.py
+++ index.py
@@ -1,16 +1,5 @@
-number = int(input("input a number. "))
-pnum = False
-npnum = False
-ranger = range(1, number + 1)
-
-if num % ranger == 0:
-    pnum == True
 
 
-if pnum == True:
-    print(str(number) + " is a prime number"
-else pnum == False:
-    print(str(number) + " is not a prime number"
 
 
        

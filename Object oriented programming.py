1f9a299044766f3e976dac30116281de971ea0d4
Catelyn-Close <catie.did75@gmail.com>
2024-09-05 12:11:59 -0700

    exported from Skill Struck

---
+++ Object oriented programming.py
@@ -0,0 +1,20 @@
+#checkpoint
+class Dog:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+#antique car show Challenge
+class Car:
+    def __init__(self, kind, color, year):
+        self.kind = kind
+        self.color = color
+        self.year = year
+#arborist Challenge
+class Tree:
+    def __init__(self, fruit, height, leaf, flower, seed):
+        self.fruit = fruit
+        self.height = height
+        self.leaf = leaf
+        self.flower = flower
+        self.seed = seed

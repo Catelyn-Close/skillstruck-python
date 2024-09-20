e589b2952f6a4f7c16c0fccf2d50c7cb16a29ffd
Catelyn-Close <catie.did75@gmail.com>
2024-09-20 12:28:14 -0700

    exported from Skill Struck

---
+++ Merge Sort.py
@@ -0,0 +1,14 @@
+#challenge
+arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+inp = int(input("Add a number: "))
+arr.append(inp)
+list1 = []
+list1.append(arr)
+
+res = list1.sort()
+print(res)
+
+
+#Not done***
+        
+    

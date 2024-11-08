835c06b3172550912a9a4af68d56eda1121f79b7
Catelyn-Close <catie.did75@gmail.com>
2024-11-08 14:08:10 -0700

    exported from Skill Struck

---
+++ Python math library.py
@@ -0,0 +1,35 @@
+#checkpoint
+import math
+
+x = math.ceil(3.2)
+print(x)
+
+y = math.floor(3.2)
+print(y)
+
+v = math.pi
+print(v)
+b = math.sqrt(81)
+print(b)
+c = pow(3, 4)
+print(c)
+#lots more stuff
+#challenges biking town
+people = int(input("how many people? " ))
+bikes = int(input("how many bikes? "))
+
+total = bikes / people
+
+math.ceil(total)
+
+print("In this town, for every bike that exists there are ", total, " people.")
+
+
+
+
+
+
+
+
+
+

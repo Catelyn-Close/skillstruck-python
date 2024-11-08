24f71aaf54042454d44527381e90c02e9c843284
Catelyn-Close <catie.did75@gmail.com>
2024-11-08 14:08:14 -0700

    exported from Skill Struck

---
+++ OS path.py
@@ -0,0 +1,15 @@
+import os
+#pulls a function from another python
+os.path.adventure()
+#gives boolean as to whether the file in question exists or not
+os.path.isfile()
+#same but for directory ie "Users"
+os.pathosdir()
+#Removes a file
+os.path.remove()
+#returns name of file from the path given
+os.path.basename()
+#gives directory name where file is located
+os.path.dirname()
+#all the modules that it can search inside 
+sys.path()

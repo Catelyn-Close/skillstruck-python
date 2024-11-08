6b61dd9c0d6532df18a6b2321d05b3bdf548b959
Catelyn-Close <catie.did75@gmail.com>
2024-11-08 14:08:12 -0700

    exported from Skill Struck

---
+++ Different Concatenating Approaches.py
@@ -0,0 +1,26 @@
+#Checkpoint
+name = "ana"
+dogs = 2
+location = "walmart"
+print(f"My name is {name} and I have {dogs} dogs and live in {location}")
+print("My name is " + name + " and I have " + str(dogs) + " dogs and live in " + location)
+print("My name is", name, "and I have", dogs, "dogs and live in", location)
+#challenges market list
+c1 = "11"
+c2 = "22"
+c3 = "33"
+c4 = "44"
+c5 = "55"
+v1 = 1
+v2 = 2
+v3 = 3
+v4 = 4
+v5 = 5
+print(f"{v1} {v2} {v3} {v4} {v5} {c1} {c2} {c3} {c4} {c5}")
+#Challenge Sports scores
+x1 = 28
+x2 = 5
+x3 = 3
+x4 = 79
+x5 = 32
+print(x1,x2,x3,x4,x5)

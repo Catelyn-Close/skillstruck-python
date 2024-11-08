d221bd8be10161b516f15d8ded26a72cb346826a
Catelyn-Close <catie.did75@gmail.com>
2024-11-08 14:08:06 -0700

    exported from Skill Struck

--- BIG BIG PROJECT rock paper scissors.py
+++ BIG BIG PROJECT rock paper scissors.py
@@ -28,11 +28,11 @@ def numsforcalc(n=0):  # n is selecting the array element
         for x,y in rules1dict.items():
             if x in allplayerb[n]:
                 rules1dict[x] = True
-    print(rules1dict) #debug
+    #print(rules1dict) #debug
 
 
 
-def calculating(reala, realb,m,l):
+def calculating(m):
     ## read the dictionary
     rock1 = rules1dict.get("rock1", False)
     paper1 = rules1dict.get("paper1", False)
@@ -41,39 +41,35 @@ def calculating(reala, realb,m,l):
     paper2 = rules1dict.get("paper2", False)
     scissors2 = rules1dict.get("scissors2", False)
     values = list(rules1dict.values())
-
+    this_score_a = 0
+    this_score_b = 0
     # calculate the player score for this round
-    if values[m] == True and values[l] == True:
-        playerascore += 1
-        playerbscore += 1
-        print(playerascore)
-        return playerascore, playerbscore
+    if values[m] == True and values[m+3] == True:
+        this_score_a += 1
+        this_score_b += 1
+        
     elif rules1dict["rock1"] and rules1dict["paper2"] == True:
-        playerbscore += 1
+        this_score_b += 1
     elif rules1dict["rock1"] and rules1dict["scissors2"] == True:
-        playerascore += 1 
+        this_score_a += 1
     elif rules1dict["paper1"] and rules1dict["rock2"] == True:
-        playerascore += 1
+        this_score_a += 1
     elif rules1dict["paper1"] and rules1dict["scissors2"] == True:
-        playerbscore += 1
+        this_score_b += 1
     elif rules1dict["scissors1"] and rules1dict["rock2"] == True:
-        playerbscore += 1
+        this_score_b += 1
     elif rules1dict["scissors1"] and rules1dict["paper2"] == True:
-        playerascore += 1
-    print("r1 stats: \n", "Player A: " playerascore, "\n", "Player B: ", playerbscore)
-    ## update the player's total score
-    reala += playerascore
-    realb += playerbscore
-    
-    return 
+        this_score_a += 1
+    ## send back this round's score
+    return (this_score_a, this_score_b)
 
 ####  State variables
 hi = 2
 a = "rock1 paper1 scissors1@scissors2 paper2 rock2"
-playerascore = 0
-playerbscore = 0
-reala=0
-realb=0
+this_score_a = 0
+this_score_b = 0
+total_score_a = 0
+total_score_b = 0
 rules1dict = {
     "rock1" : False,
     "paper1" : False,
@@ -88,84 +84,68 @@ rules1dict = {
 
 # User input and set up
 
-write = input("input both sides")
+write = input("input both sides: ")
 moves = write.split('@') #creates a 2 element array, player 1 in element 0
 allplayera = moves[0].split() # create a 3 element array for P1 moves
 allplayerb = moves[1].split()  # create a 3 element array for P2 moves
 
 
+# Round 1, compare array elements[0]
+m, l, n  = 0, 0, 0
+numsforcalc(n)
+this_round_score = calculating(m)
+
+#print("r1 stats")
+total_score_a += this_round_score[0]
+total_score_b += this_round_score[1]
+#print("Total Player 1; ", total_score_a)
+#print("Total Player 2; ",total_score_b)
+#print("--------")
 
 
-# print("NUMS!!!!!!!") #debug
-# print("START!!!!!!!!!") #debug
-# print(allplayera[1]) #debug
-# print(allplayerb[1]) #debug
-
+# Round 1, compare array elements[0]
+m, l, n  = 1, 1, 1
+numsforcalc(n)
+this_round_score = calculating(m)
+total_score_a += this_round_score[0]
+total_score_b += this_round_score[1]
+#print("r2 stats")
+#print("Total Player 1; ", total_score_a)
+#print("Total Player 2; ",total_score_b)
+#print("--------")
 
 
 
 # Round 1, compare array elements[0]
-numsforcalc()
-calculating(reala, realb, m, l)
+m, l, n  = 2, 2, 2
+numsforcalc(n)
+this_round_score = calculating(m)
+total_score_a += this_round_score[0]
+total_score_b += this_round_score[1]
+#print("r3 stats")
+#print("Total Player 1; ", total_score_a)
+#print("Total Player 2; ",total_score_b)
+#print("--------")
 
 
 
-print(reala)
-print(realb)
-print("r2 stats")
-numsforcalc(1)
-calculating(playerascore, playerbscore,1, 1)
-print(reala)
-print(realb)
-print("r3 stats")
-numsforcalc(n=2)
-calculating(playerascore=0, playerbscore=0,m=2, l=2)
-print(reala)
-print(realb)
-
-
-
-#tying is broke currently
+#tying is fixed :)
 
 
 
 
 these = """
+variation 1
 rock1 paper1 scissors1@scissors2 paper2 rock2
-
+variation 2
 paper1 rock1 scissors1@scissors2 paper2 rock2
 """
 
-stuf = """
-if not hasattr(calculating, first_run):
-        reala=0
-        realb=0
-        calculating.first_run = 0
-        print("ITS WORKING!!!!!!!!!??????????!!!!!!???")
 
-
-pla = ["one1", "two2", "three3"]
-
-plab = ["one", "two", "three"]
-dictpla = {
-    "one" : True, 
-    "two" : False, 
-    "three" : False,
-    "one1" : True,
-    "two2" : False,
-    "three3" : False
-}
-plas = 0
-plabs = 0
-m = 0
-l =3
-values = list(rules1dict.values())
-if values[m] == True and values[l] == True:
-    plas += 1
-    plabs += 1
+if total_score_a > total_score_b:
+    print(total_score_a, total_score_b, "Player 1 Wins!")
+elif total_score_a < total_score_b:
+    print(total_score_a, total_score_b, "Player 2 Wins!")
 else:
-    print(":(")
+    print(total_score_a, total_score_b, "Tie!")
 
-print(plas, plabs)
-
-"""
